ec869ac95f59584f0dfd8f884c8b3dd2eff4dfac
Leonardo Rosi <leonardo.rosi.dev@gmail.com>
2026-01-22 11:10:43 +0100

    primo push

---
+++ operazioni.py
@@ -0,0 +1,14 @@
+def somma ( a:float, b:float ):
+  if isinstance(a, (int,float) ) and isinstance(b, (int,float) ):
+    return a + b
+  else:
+    #raise TypeError("a e b devono essere numeri")
+    # print("a e b devono essere numeri")
+    #return TypeError("a e b devono essere numeri")
+    return None
+  
+def sottrazione(a: float, b: float):
+    if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+        return a / b
+    else:
+        return None
